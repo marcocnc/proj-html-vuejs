e4d6967f0a9f7f98506a6ab642c4046533b738c1
marcocnc <marcocancelliere56@gmail.com>
2023-05-11 17:34:18 +0200

    implemented loading

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,47 @@
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 import Footer from './components/Footer.vue';
-
+import Loading from './components/partials/Loading.vue';
+import {store} from './data/store';
 
 export default {
     name:'App',
     components:{
       Header,
       Main,
-      Footer
+      Footer,
+      Loading
+    },
+
+    data(){
+      return{
+        store
+      }
     },
 
     methods:{
       getImage(img){
         return new URL(img, import.meta.url).href;
+      },
+
+      stopLoading(){
+        setInterval(() => {
+          store.isLoading = false;
+        }, 4000);
       }
+    },
+
+    mounted(){
+      this.stopLoading();
     }
 }
 </script>
 
 <template>
+  <Loading v-if="store.isLoading"/>
 
     <Header />
+
 
     <Main />
 
---
+++ src/components/partials/Loading.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+    name: 'Loading'
+}
+</script>
+
+<template>
+    <div class="loading-layer d-flex justify-content-center align-items-center">
+        <div class="lds-ripple"><div></div><div></div></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../scss/partials/vars' as *;
+
+    .loading-layer{
+        height: 100vh;
+        background-color: $color_red;
+        position: fixed;
+        z-index: 999;
+        width: 100%;
+    }
+    .lds-ripple {
+        display: inline-block;
+        position: relative;
+        width: 80px;
+        height: 80px;
+    }
+    .lds-ripple div {
+        position: absolute;
+        border: 4px solid #fff;
+        opacity: 1;
+        border-radius: 50%;
+        animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+    }
+    .lds-ripple div:nth-child(2) {
+        animation-delay: -0.5s;
+    }
+
+    @keyframes lds-ripple {
+    0% {
+        top: 36px;
+        left: 36px;
+        width: 0;
+        height: 0;
+        opacity: 0;
+    }
+    4.9% {
+        top: 36px;
+        left: 36px;
+        width: 0;
+        height: 0;
+        opacity: 0;
+    }
+    5% {
+        top: 36px;
+        left: 36px;
+        width: 0;
+        height: 0;
+        opacity: 1;
+    }
+    100% {
+        top: 0px;
+        left: 0px;
+        width: 72px;
+        height: 72px;
+        opacity: 0;
+    }
+    }
+</style>
